44538d9418de27293490344d02e2f23430245e27
ariefky <48465565+ariefky@users.noreply.github.com>
2019-03-26 12:06:01 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,25 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LatihanMemberClass
+namespace LatihanClassdanObject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            //Membuat objek dari class
-            TaxiOnline taxi = new TaxiOnline();
+            //Membuat objek dari class Mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
 
-            //Mengeset nilai properties
-            taxi.DriverName = "Bemo";
-            taxi.OnDuty = true;
-            taxi.NumPassenger = 1;
+            //Mengeset nilai properties objek saya dan kamu
+            saya.Nim = "131415";
+            saya.Nama = "Paijo";
+            saya.Ipk = 3.95f;
 
-            //Memanggil method
-            taxi.TaxiInfo();
-            taxi.Menjemput();
-            taxi.Mengantar();
+            kamu.Nim = "121314";
+            kamu.Nama = "Bemo";
+            kamu.Ipk = 4f;
+
+            //Memanggil method registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
 
             Console.ReadKey();
         }
